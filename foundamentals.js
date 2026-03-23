3dd5ac682df64fbdd6d1c9d5ee380e924b8102c9
Munguromo Irene <munguromoirene7@gmail.com>
2026-03-23 20:45:24 +0300

    first commit

---
+++ foundamentals.js
@@ -0,0 +1,89 @@
+let colorOfLight = "yellow"
+if(colorOfLight === "green") {
+    console.log("The traffic light is currently green")
+} else if (colorOfLight === "red"){
+    console.log("The traffic is red, please stop")
+} else {
+    console.log("The traffic light is currently yellow")
+}
+
+let reportedAge = 18
+let gender ="M"
+if (reportedAge >= 18) {
+    console.log("These are adults")
+    if(gender=="M"){
+        console.log("These are men")
+    } else{
+        console.log("These are women")
+    } else if(gender = "LGBTQ")
+} else {
+    console.log("These are children")
+}
+
+let dayOfWeek = 0
+switch (dayOfWeek){
+    case 0: console.log("Today is Monday");
+    break;
+    case 1: console.log("Today is Tuesday");
+    break;
+    case 2: console.log("Today is Wednesday");
+    break;
+    default: console.log("Today is not known");
+    break
+}
+
+let timeOfDay = "0800";
+let customWelcomeMessage
+switch (timeOfDay){
+    case "0800" : customWelcomeMessage="Welcome, you night owl";
+    break;
+    case "0800": customWelcomeMessage="Its not true, the early bird doesnot catch the worm";
+    break;
+    case "1200" : customWelcomeMessage="Good afternoon, i hope you have lunch money";
+    break;
+    default: customWelcomeMessage="Please fix you clock";
+    break
+}
+
+customWelcomeMessage = timeOfDay == "1200" ? "Good morning, I hope you are a great day" : "Have a good evening";
+console.log(customWelcomeMessage);
+
+let discount = gender === "F" ? 0.4 : 0.1;
+let priceOfCurtains = 45000;
+
+console.log("The final price the ${gender ===")
+
+
+// *assingement
+let buyerName = "Irene M.";
+let amountDue = "10000";
+let itemName = "nails";
+
+if (buyerName.length>=2 && amountDue.length>=5 && itemName.length>0){
+console.log("record is valid")
+}else{
+    console.log(
+    "record is invalid"
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,303 @@
+
+// // console.log("Hello World","This is the class of march 2006","We are learning JavaScript");
+// //this is console
+
+// /** */
+
+// // console. log('Hello Wolrd')
+// // console. log(`this is a back tick`)
+// // console. log(23)
+// // console. log(0.25)
+
+// // console.log(true)
+// // console.log(false)
+
+// // console.log("The sum of 10 and 4 is",10+4)
+// // console.log("The sum of 10.384 and 6.864 is",10.384+6.864)
+
+// // console.log("The difference of 10 and 6",10-6)
+
+// // let number1=5
+// // let number2=8
+// // console.log(number1-number2)
+
+// // let firstName= "Munguromo"
+// // let lastName= "Irene"
+
+// // console.log( PI=22/7)
+
+// // console.log((40*2)+(18-5))
+
+// // const PI=22/7
+
+// // let age = "23"
+// // console.log(typeof age)
+// // age = parseInt(age)
+// // console.log(typeof age)
+
+
+
+
+// // let phoneNumber = 75321937
+// // phoneNumber = phoneNumber.toString
+// // console.log(phoneNumber)
+
+
+// let amount = "5000"
+// let amountFromDB = 5000
+// // console.log(amount == amountFromDB)
+
+// // console.log(amount === amountFromDB)
+
+// let userCredentials = null
+// console.log(userCredentials === null)
+
+// console.log(amount != amounFromDB)
+
+// let isMan = gender === "M" &&  reported >= 18
+
+// let isAPublicHoliday = false
+// let isAWeekend = false
+
+// // console.log(isAPublicHoliday || isAWeekend)
+
+// // console.log((gender === "M && reportedAge >= 18") || (isAPublicHoliday || isAWeekend))
+
+// let isNotLoggedIn = !userCredentials
+// console.log(isNotLoggedIn)
+
+// let userRole = "admin"
+
+// console.log(userRole==="admin")
+// ex. 1
+// let stockQuality = 100;
+// let minRequiremment = 10000;
+
+// console.log(stockQuality >= minRequiremment);
+
+// // ex. 2
+// let cost="5000";
+// let numberCost= 5000;
+// // console.log(cost == numberCost);
+
+// console.log(cost === numberCost);
+// because it checks for value and type , making it easier to spot bugs;
+// ex. 3
+// let supplierName = "Mirene";
+// let costUgx = 10000;
+// let minRecord = supplierName.length >= 2 && costUgx >= 10000;
+// console.log(minRecord)
+
+// let firstName = null
+// console.log(firstName)
+
+// let colorOfLight = "yellow"
+// if(colorOfLight === "green") {
+//     console.log("The traffic light is currently green")
+// } else if (colorOfLight === "red"){
+//     console.log("The traffic is red, please stop")
+// } else {
+//     console.log("The traffic light is currently yellow")
+// }
+
+// let reportedAge = 18
+// let gender ="M"
+// if (reportedAge >= 18) {
+//     console.log("These are adults")
+//     if(gender=="M"){
+//         console.log("These are men")
+//     } else{
+//         console.log("These are women")
+//     } else if(gender = "LGBTQ")
+// } else {
+//     console.log("These are children")
+// }
+
+// let dayOfWeek = 0
+// switch (dayOfWeek){
+//     case 0: console.log("Today is Monday");
+//     break;
+//     case 1: console.log("Today is Tuesday");
+//     break;
+//     case 2: console.log("Today is Wednesday");
+//     break;
+//     default: console.log("Today is not known");
+//     break
+// }
+
+// let timeOfDay = "0800";
+// let customWelcomeMessage
+// switch (timeOfDay){
+//     case "0800" : customWelcomeMessage="Welcome, you night owl";
+//     break;
+//     case "0800": customWelcomeMessage="Its not true, the early bird doesnot catch the worm";
+//     break;
+//     case "1200" : customWelcomeMessage="Good afternoon, i hope you have lunch money";
+//     break;
+//     default: customWelcomeMessage="Please fix you clock";
+//     break
+// }
+
+// customWelcomeMessage = timeOfDay == "1200" ? "Good morning, I hope you are a great day" : "Have a good evening";
+// console.log(customWelcomeMessage);
+
+// let discount = gender === "F" ? 0.4 : 0.1;
+// let priceOfCurtains = 45000;
+
+// console.log("The final price the ${gender ===")
+
+let buyerName = "Irene M.";
+let amountDue = "10000";
+let itemName = "nails";
+
+if (buyerName.length>=2 && amountDue.length>=5 && itemName.length>0){
+console.log("record is valid")
+}else{
+    console.log(
+    "record is invalid"
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
